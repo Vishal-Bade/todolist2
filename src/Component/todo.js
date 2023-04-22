3762d7088300c1b108284dc5c1fe2e81cdfc43db
Vishal-Bade <vishalbade1997@gmail.com>
2023-04-22 08:24:56 +0530

    todo list project 2 version 2

--- src/Component/todo.js
+++ src/Component/todo.js
@@ -4,20 +4,49 @@ import img from "../todo_img.png";
 const Todo = () => {
   const [inputData, setInputData] = useState("");
   const [items, setItems] = useState([]);
+  const [toggleSubmit, setToggleSubmit] = useState(true);
+  const [isEditItem, setIsEditItem] = useState(null);
 
   const addItem = () => {
     if (!inputData) {
+      alert("plzz Fill the data.");
+    } else if (inputData && !toggleSubmit) {
+      setItems(
+        items.map((elem) => {
+          if (elem.id === isEditItem) {
+            return { ...elem, name: inputData };
+          }
+          return elem;
+        })
+      );
+      setToggleSubmit(true);
+      setInputData("");
+      setIsEditItem(null);
     } else {
-      setItems([...items, inputData]);
+      const allInputData = {
+        id: new Date().getTime().toString(),
+        name: inputData,
+      };
+      setItems([...items, allInputData]);
       setInputData("");
     }
   };
 
-  const deleteItem = (id) => {
-    const updateItems = items.filter((elem, ind) => {
-      return ind !== id;
+  const deleteItem = (index) => {
+    const updateItems = items.filter((elem) => {
+      return index !== elem.id;
     });
     setItems(updateItems);
+  };
+
+  const editItem = (id) => {
+    let newEditItem = items.find((elem) => {
+      return elem.id === id;
+    });
+    // console.log(newEditItem);
+    setToggleSubmit(false);
+    setInputData(newEditItem.name);
+    setIsEditItem(id);
   };
 
   return (
@@ -36,27 +65,46 @@ const Todo = () => {
               value={inputData}
               onChange={(e) => setInputData(e.target.value)}
             />
-            <span
-              className="material-symbols-outlined add_btn"
-              title="Add Item"
-              onClick={addItem}
-            >
-              add
-            </span>
+            {toggleSubmit ? (
+              <span
+                className="material-symbols-outlined add_btn"
+                title="Add Item"
+                onClick={addItem}
+              >
+                add
+              </span>
+            ) : (
+              <span
+                className="material-symbols-outlined edit_btn"
+                title="Update Item"
+                onClick={addItem}
+              >
+                edit
+              </span>
+            )}
           </div>
 
           <div className="showItems">
-            {items.map((elem, ind) => {
+            {items.map((elem) => {
               return (
-                <div className="eachItem" key={ind}>
-                  <h3>{elem}</h3>
-                  <span
-                    className="material-symbols-outlined deleteItem"
-                    title="Delete Item"
-                    onClick={() => deleteItem(ind)}
-                  >
-                    delete
-                  </span>
+                <div className="eachItem" key={elem.id}>
+                  <h3>{elem.name}</h3>
+                  <div className="todo-btn">
+                    <span
+                      className="material-symbols-outlined edit_btn"
+                      title="Edit Item"
+                      onClick={() => editItem(elem.id)}
+                    >
+                      edit
+                    </span>
+                    <span
+                      className="material-symbols-outlined delete_btn"
+                      title="Delete Item"
+                      onClick={() => deleteItem(elem.id)}
+                    >
+                      delete
+                    </span>
+                  </div>
                 </div>
               );
             })}
@@ -70,7 +118,7 @@ const Todo = () => {
               title="Remove All"
               onClick={() => setItems([])}
             >
-              <span> REMOVE ALL </span>
+              <span> Remove </span>
             </button>
           </div>
         </div>
